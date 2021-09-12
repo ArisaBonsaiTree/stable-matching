48369bc36eb7ff215f7dc7809733762a47ba4f32
ArsiaBonsaiTree <angelevarela@hotmail.com>
2021-09-12 12:28:09 -0700

    Use of objects helps with the program New branch main

---
+++ src/Student.java
@@ -0,0 +1,48 @@
+// Comment
+
+
+public class Student {
+    String name;
+    boolean isTaken = false;
+    String[] choices;
+    String takenBy;
+    int startCount = 0;
+
+    public Student(String name, boolean isTaken, String[] choices, String takenBy) {
+        this.name = name;
+        this.isTaken = isTaken;
+        this.choices = choices;
+        this.takenBy = takenBy;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isTaken() {
+        return isTaken;
+    }
+
+    public void setTaken(boolean taken) {
+        isTaken = taken;
+    }
+
+    public String getChoices(int n) {
+        return choices[n];
+    }
+
+    public void setChoices(int n, String input) {
+        choices[n] = input;
+    }
+
+    public String getTakenBy() {
+        return takenBy;
+    }
+
+    public void setTakenBy(String takenBy) {
+        this.takenBy = takenBy;
+    }
+}
